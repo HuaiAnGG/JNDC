f3d841f95b549fdcab12d346716f3cc6aa60ecca
HuaiAnGG <971375353@qq.com>
2019-05-06 02:11:03 +0800

    remove the module_girls

--- module_girls/src/main/java/google/architecture/girls/GirlItemClickCallback.java
+++
@@ -1,9 +0,0 @@
-
-package google.architecture.girls;
-
-
-import google.architecture.coremodel.datamodel.http.entities.GirlsData;
-
-public interface GirlItemClickCallback {
-    void onClick(GirlsData.ResultsBean girlsItem);
-}
